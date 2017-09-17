7966818c52e9098b29021e8d5c5c643e1b41bed8
J Fernandez <jffernan@comcast.net>
2017-09-17 22:00:33 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-// Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const madMaxGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img
+          src={this.props.poster}
+          alt={this.props.title}
+        />
+        <h2>{this.props.title}</h2>
+        <small>Genres: {this.props.genres.join(', ')}</small>
+      </div>
+    );
+  }
+}
+
+MovieCard.defaultProps = {
+  poster: 'http://i.imgur.com/bJw8ndW.png'
+};
+
+ReactDOM.render(
+  <MovieCard
+    title="Mad Max: Fury Road"
+    poster="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg"
+    genres={madMaxGenres}
+  />,
+  document.getElementById('root')
+);
+
+const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+
+ReactDOM.render(
+  <MovieCard
+    title="Jurassic World"
+    genres={jurassicWorldGenres}
+  />,
+  document.getElementById('root')
+);
