1325800b393bddc6b8f75a13c2ecd1621afb0507
James Fernandez <jffernan@comcast.net>
2017-11-08 14:35:08 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const madMaxGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+const{poster, title, genres}=this.props
 
 class MovieCard extends React.Component {
   render() {
     return (
       <div className="movie-card">
         <img
-          src={this.props.poster}
-          alt={this.props.title}
+          src={poster}
+          alt={title}
         />
-        <h2>{this.props.title}</h2>
-        <small>Genres: {this.props.genres.join(', ')}</small>
+        <h2>{title}</h2>
+        <small>Genres: {genres.join(', ')}</small>
       </div>
     );
   }
