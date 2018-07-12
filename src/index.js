2eb0fe58d7dcc6a60238bc004e4627bc914e142e
Alli Tzadok <allisontzadok@gmail.com>
2018-07-12 15:17:22 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 // Code goes here
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import MovieCard from './components/moviecard';
+
+const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+
+ReactDOM.render(
+  <MovieCard
+    title="Jurassic World"
+    poster="http://image.tmdb.org/t/p/w342/jjBgi2r5cRt36xF6iNUEhzscEcb.jpg"
+    genres={jurassicWorldGenres}
+  />,
+  document.getElementById('root')
+);
